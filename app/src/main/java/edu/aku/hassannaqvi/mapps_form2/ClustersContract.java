d6f5fd1ee2e4accf033a14c73b188a21d8a9e5b4
aliazaz <openmind_ali@hotmail.com>
2017-02-27 15:16:34 +0500

    Contract Updated

--- app/src/main/java/edu/aku/hassannaqvi/mapps_form2/ClustersContract.java
+++ app/src/main/java/edu/aku/hassannaqvi/mapps_form2/ClustersContract.java
@@ -1,5 +1,6 @@
 package edu.aku.hassannaqvi.mapps_form2;
 
+import android.database.Cursor;
 import android.provider.BaseColumns;
 
 import org.json.JSONException;
@@ -21,13 +22,53 @@ public class ClustersContract {
         // Default Constructor
     }
 
+    public ClustersContract Sync(JSONObject jsonObject) throws JSONException {
+        this._ID= jsonObject.getLong(singleCluster._ID);
+        this.clusterId= jsonObject.getString(singleCluster.CLUSTERID);
+        this.clusterName= jsonObject.getString(singleCluster.CLUSTERNAME);
+
+        return this;
+
+    }
+
+    public ClustersContract Hydrate(Cursor cursor) {
+        this._ID = cursor.getLong(cursor.getColumnIndex(singleCluster._ID));
+        this.clusterId = cursor.getString(cursor.getColumnIndex(singleCluster.CLUSTERID));
+        this.clusterName = cursor.getString(cursor.getColumnIndex(singleCluster.CLUSTERNAME));
+
+        return this;
+    }
+
+    public Long get_ID() {
+        return _ID;
+    }
+
+    public void set_ID(Long _ID) {
+        this._ID = _ID;
+    }
+
+    public String getClusterId() {
+        return clusterId;
+    }
+
+    public void setClusterId(String clusterId) {
+        this.clusterId = clusterId;
+    }
+
+    public String getClusterName() {
+        return clusterName;
+    }
+
+    public void setClusterName(String clusterName) {
+        this.clusterName = clusterName;
+    }
 
     public JSONObject toJSONObject() throws JSONException {
 
         JSONObject json = new JSONObject();
         json.put(singleCluster._ID, this._ID == null ? JSONObject.NULL : this._ID);
-
-
+        json.put(singleCluster.CLUSTERID, this.clusterId == null ? JSONObject.NULL : this.clusterId);
+        json.put(singleCluster.CLUSTERNAME, this.clusterName == null ? JSONObject.NULL : this.clusterName);
         return json;
     }
 
@@ -35,8 +76,9 @@ public class ClustersContract {
     public static abstract class singleCluster implements BaseColumns {
 
         public static final String TABLE_NAME = "clusters";
-        public static final String _ID = "id";
-
+        public static final String _ID = "_id";
+        public static final String CLUSTERID = "clusterid";
+        public static final String CLUSTERNAME = "clustername";
 
         public static final String _URI = "getclusters.php";
     }
--- app/src/main/java/edu/aku/hassannaqvi/mapps_form2/LHWsContract.java
+++ app/src/main/java/edu/aku/hassannaqvi/mapps_form2/LHWsContract.java
@@ -1,9 +1,12 @@
 package edu.aku.hassannaqvi.mapps_form2;
 
+import android.database.Cursor;
 import android.provider.BaseColumns;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import org.json.*;
 
 /**
  * Created by hassan.naqvi on 11/30/2016.
@@ -23,14 +26,76 @@ public class LHWsContract {
         // Default Constructor
     }
 
+    public Long get_ID() {
+        return _ID;
+    }
+
+    public void set_ID(Long _ID) {
+        this._ID = _ID;
+    }
+
+    public String getLhwId() {
+        return lhwId;
+    }
+
+    public void setLhwId(String lhwId) {
+        this.lhwId = lhwId;
+    }
+
+    public String getLhwName() {
+        return lhwName;
+    }
+
+    public void setLhwName(String lhwName) {
+        this.lhwName = lhwName;
+    }
+
+    public String getClusterName() {
+        return clusterName;
+    }
+
+    public void setClusterName(String clusterName) {
+        this.clusterName = clusterName;
+    }
+
+    public String getClusterCode() {
+        return clusterCode;
+    }
+
+    public void setClusterCode(String clusterCode) {
+        this.clusterCode = clusterCode;
+    }
 
     public JSONObject toJSONObject() throws JSONException {
 
         JSONObject json = new JSONObject();
         json.put(singleLhw._ID, this._ID == null ? JSONObject.NULL : this._ID);
-
-
+        json.put(singleLhw.LHWID, this.lhwId == null ? JSONObject.NULL : this.lhwId);
+        json.put(singleLhw.LHWNAME, this.lhwName == null ? JSONObject.NULL : this.lhwName);
+        json.put(singleLhw.CLUSTERNAME, this.clusterName == null ? JSONObject.NULL : this.clusterName);
+        json.put(singleLhw.CLUSTERCODE, this.clusterCode == null ? JSONObject.NULL : this.clusterCode);
         return json;
+    }
+
+    public LHWsContract Sync(JSONObject jsonObject) throws JSONException {
+        this._ID= jsonObject.getLong(singleLhw._ID);
+        this.lhwId= jsonObject.getString(singleLhw.LHWID);
+        this.lhwName= jsonObject.getString(singleLhw.LHWNAME);
+        this.clusterName= jsonObject.getString(singleLhw.CLUSTERNAME);
+        this.clusterCode= jsonObject.getString(singleLhw.CLUSTERCODE);
+
+        return this;
+
+    }
+
+    public LHWsContract Hydrate(Cursor cursor) {
+        this._ID = cursor.getLong(cursor.getColumnIndex(singleLhw._ID));
+        this.lhwId = cursor.getString(cursor.getColumnIndex(singleLhw.LHWID));
+        this.lhwName = cursor.getString(cursor.getColumnIndex(singleLhw.LHWNAME));
+        this.clusterName = cursor.getString(cursor.getColumnIndex(singleLhw.CLUSTERNAME));
+        this.clusterCode = cursor.getString(cursor.getColumnIndex(singleLhw.CLUSTERCODE));
+
+        return this;
     }
 
 
@@ -38,6 +103,10 @@ public class LHWsContract {
 
         public static final String TABLE_NAME = "lhws";
         public static final String _ID = "id";
+        public static final String LHWID = "lhwid";
+        public static final String LHWNAME = "lhwname";
+        public static final String CLUSTERNAME = "clustername";
+        public static final String CLUSTERCODE = "clustercode";
 
 
         public static final String _URI = "getlhws.php";
