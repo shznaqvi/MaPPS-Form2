4646edcac82a1beefcdab9ce79e3138886739671
gul.sanober <gulsanobermemon@gmail.com>
2017-06-19 13:54:11 +0500

    Multiple activity changes

--- app/src/main/java/edu/aku/hassannaqvi/mapps_form2/activities/SectionCHActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/mapps_form2/activities/SectionCHActivity.java
@@ -204,12 +204,22 @@ public class SectionCHActivity extends Activity {
         ButterKnife.bind(this);
 
 
-        if (AppMain.maritalStatus != 1) {
+        /*if (AppMain.maritalStatus != 1) {
             fldGrpmp02ch005.setVisibility(View.GONE);
             mp02ch005.clearCheck();
+            mp02ch006.clearCheck();
+            mp02ch00701.setText(null);
+            mp02ch00702.setText(null);
+            mp02ch00801.setChecked(false);
+            mp02ch00802.setChecked(false);
+            mp02ch00803.setChecked(false);
+            mp02ch00804.setChecked(false);
+            mp02ch00888.setChecked(false);
+            mp02ch00888x.se
+
         } else {
             fldGrpmp02ch005.setVisibility(View.VISIBLE);
-        }
+        }*/
 
         mp02ch001.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
@@ -227,6 +237,8 @@ public class SectionCHActivity extends Activity {
                     mp02ch00404.setChecked(false);
                     mp02ch00488.setChecked(false);
                     mp02ch00488x.setText(null);
+
+
                 }
             }
         });
@@ -519,7 +531,7 @@ public class SectionCHActivity extends Activity {
 
 //        5
 
-        if (AppMain.maritalStatus == 1) {
+        //if (AppMain.maritalStatus == 1) {
             if (mp02ch005.getCheckedRadioButtonId() == -1) {
                 Toast.makeText(this, "ERROR(empty)" + getString(R.string.mp02ch005), Toast.LENGTH_SHORT).show();
                 mp02ch00599.setError("This data is Required!");
@@ -529,7 +541,7 @@ public class SectionCHActivity extends Activity {
             } else {
                 mp02ch00599.setError(null);
             }
-        }
+        //}
 
 //        Skip
         if (mp02ch00501.isChecked()) {
@@ -620,92 +632,6 @@ public class SectionCHActivity extends Activity {
         }
 
         //====================================
-
-        if (AppMain.maritalStatus == 1) {
-
-//            6
-            if (mp02ch006.getCheckedRadioButtonId() == -1) {
-                Toast.makeText(this, "ERROR(empty)" + getString(R.string.mp02ch006), Toast.LENGTH_SHORT).show();
-                mp02ch00605.setError("This data is Required!");
-
-                Log.i(TAG, "mp02ch006: This data is Required!");
-                return false;
-            } else {
-                mp02ch00605.setError(null);
-            }
-
-//            7
-            if (mp02ch00701.getText().toString().isEmpty()) {
-                Toast.makeText(this, "ERROR(empty)" + getString(R.string.mp02ch007m), Toast.LENGTH_SHORT).show();
-                mp02ch00701.setError("This data is Required!");
-
-                Log.i(TAG, "mp02ch007: This data is Required!");
-                return false;
-            } else {
-                mp02ch00701.setError(null);
-            }
-
-            if (mp02ch00702.getText().toString().isEmpty()) {
-                Toast.makeText(this, "ERROR(empty)" + getString(R.string.mp02ch007d), Toast.LENGTH_SHORT).show();
-                mp02ch00702.setError("This data is Required!");
-
-                Log.i(TAG, "mp02ch00702: This data is Required!");
-                return false;
-            } else {
-                mp02ch00702.setError(null);
-            }
-
-            if ((Integer.parseInt(mp02ch00701.getText().toString().isEmpty() ? "0" : mp02ch00701.getText().toString()) < 0
-                    || Integer.parseInt(mp02ch00701.getText().toString().isEmpty() ? "0" : mp02ch00701.getText().toString()) > 11)
-                    && (Integer.parseInt(mp02ch00702.getText().toString().isEmpty() ? "0" : mp02ch00702.getText().toString()) < 0
-                    || Integer.parseInt(mp02ch00702.getText().toString().isEmpty() ? "0" : mp02ch00702.getText().toString()) > 29)) {
-                Toast.makeText(this, "Invalid:" + getString(R.string.mp02ch007), Toast.LENGTH_SHORT).show();
-                mp02ch00701.setError("Invalid:Month range 0 - 11");
-                mp02ch00702.setError("Invalid:Days range 0 - 29");
-
-                Log.i(TAG, "mp02ch007: Month Range 0 - 11");
-                return false;
-            } else {
-                mp02ch00701.setError(null);
-                mp02ch00702.setError(null);
-            }
-
-            if (Integer.valueOf(mp02ch00701.getText().toString().isEmpty() ? "0" : mp02ch00701.getText().toString()) == 0
-                    && Integer.valueOf(mp02ch00702.getText().toString().isEmpty() ? "0" : mp02ch00702.getText().toString()) == 0) {
-                Toast.makeText(this, "ERROR(invalid)" + getString(R.string.mp02ch007) + " - " + getString(R.string.month), Toast.LENGTH_SHORT).show();
-                mp02ch00701.setError("Days and months can not be zero..");
-                mp02ch00702.setError("Days and months can not be zero..");
-
-                Log.i(TAG, "mp02cg00101: Both can not be zero");
-                return false;
-            } else {
-                mp02ch00701.setError(null);
-                mp02ch00702.setError(null);
-            }
-
-
-//            8
-            if (!(mp02ch00801.isChecked() || mp02ch00802.isChecked() || mp02ch00803.isChecked() || mp02ch00804.isChecked()
-                    || mp02ch00888.isChecked())) {
-                Toast.makeText(this, "ERROR(empty)" + getString(R.string.mp02ch008), Toast.LENGTH_SHORT).show();
-                mp02ch00888.setError("This data is Required!");
-
-                Log.i(TAG, "mp02ch008: This data is Required!");
-                return false;
-            } else {
-                mp02ch00888.setError(null);
-            }
-
-            if (mp02ch00888.isChecked() && mp02ch00888x.getText().toString().isEmpty()) {
-                Toast.makeText(this, "ERROR(empty)" + getString(R.string.mp02ch008), Toast.LENGTH_SHORT).show();
-                mp02ch00888x.setError("This data is Required!");
-
-                Log.i(TAG, "mp02ch00888x: This data is Required!");
-                return false;
-            } else {
-                mp02ch00888x.setError(null);
-            }
-        }
 
 
         //====================================
